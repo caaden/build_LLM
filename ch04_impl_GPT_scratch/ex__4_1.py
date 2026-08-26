dcdb4d9d68ffe8424ace816beae249b514ebe8b2
caaden <chrisaaden@gmail.com>
2026-08-26 16:57:40 -0400

    ch4 init

---
+++ ch04_impl_GPT_scratch/ex__4_1.py
@@ -0,0 +1,82 @@
+#%% Dependencies
+import torch
+import torch.nn as nn
+import tiktoken
+
+#%% Config Dictionary
+GPT_CONFIG_124M = {
+    "vocab_size": 50257, 
+    "context_length": 1024,
+    "emb_dim": 768,
+    "n_heads": 12,
+    "n_layers": 12,
+    "drop_rate": 0.1,
+    "qkv_bias": False
+}
+
+#%% Dummy GPT Model
+class DummyGPTModel(nn.Module):
+    '''
+    A dummy GPT model for development purposes. 
+    '''
+    def __init__(self,cfg):
+        super().__init__()
+        # Define the model components
+        self.tok_emb=nn.Embedding(cfg["vocab_size"],cfg["emb_dim"]) # input token embedding
+        self.pos_emb=nn.Embedding(cfg["context_length"], cfg["emb_dim"]) # position embedding
+        self.drop_emb=nn.Dropout(cfg['drop_rate']) # dropout layer
+        self.trf_blocks=nn.Sequential( # stack of transformer blocks
+            *[DummyTransformerBlock(cfg) for _ in range(cfg["n_layers"])]
+        )
+        self.final_norm=DummyLayerNorm(cfg["emb_dim"]) # final layer normalization
+        self.out_head=nn.Linear( # output head for logits
+            cfg['emb_dim'], cfg['vocab_size'], bias=False
+        )
+
+    def forward(self, in_idx):
+        # Forward pass through the dummy GPT model
+        batch_size, seq_len = in_idx.shape
+        tok_embeds=self.tok_emb(in_idx)
+        pos_embeds=self.pos_emb(
+            torch.arange(seq_len, device=in_idx.device)
+        )
+        x=tok_embeds+pos_embeds
+        x=self.drop_emb(x)
+        x=self.trf_blocks(x)
+        x=self.final_norm(x)
+        logits=self.out_head(x)
+        return logits
+
+class DummyTransformerBlock(nn.Module):
+    def __init__(self,cfg):
+        super().__init__()
+
+    def forward(self,x):
+        return x
+
+class DummyLayerNorm(nn.Module):
+    def __init__(self, normalized_shape, eps=1e-5):
+        super().__init__()
+    def forward(self,x):
+        return x
+
+#%% Tokenize input text and create a batch
+tokenizer = tiktoken.get_encoding("gpt2")
+batch=[]
+txt1="Every effort moves you"
+txt2="Every day holds a"
+
+batch.append(torch.tensor(tokenizer.encode(txt1)))
+batch.append(torch.tensor(tokenizer.encode(txt2)))
+batch=torch.stack(batch, dim=0)
+print(batch)
+
+# %% Initialize dummy GPT model
+torch.manual_seed(123)
+model=DummyGPTModel(GPT_CONFIG_124M)
+logits=model(batch)
+print('Output shape:', logits.shape)
+print('Logits:', logits)
+
+
+# %%
